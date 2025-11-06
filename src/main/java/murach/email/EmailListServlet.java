b20500e636baaeb9ea1287859ee1a814d164000e
Yuuu Bin <daonguyennhatanh0910@gmail.com>
2025-11-06 20:51:43 +0700

    'update'

---
+++ src/main/java/murach/email/EmailListServlet.java
@@ -0,0 +1,77 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+package murach.email;
+
+import java.io.IOException;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+// Import các lớp kinh doanh và dữ liệu
+import murach.business.User;
+//import murach.data.UserDB;
+
+
+/**
+ *
+ * @author Admin
+ */
+@WebServlet(name = "EmailListServlet", urlPatterns = {"/emailList"}) 
+public class EmailListServlet extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String url = "/index.html";
+
+        // get current action
+        String action = request.getParameter("action");
+        if (action == null) {
+            action = "join";  // default action
+        }
+        // perform action and set URL to appropriate page
+        if (action.equals("join")) {
+            url = "/index.html";    // the "join" page
+        }
+        else if (action.equals("add")) {                
+            // get parameters from the request
+            String firstName = request.getParameter("firstName");
+            String lastName = request.getParameter("lastName");
+            String email = request.getParameter("email");
+
+            // store data in User object and save User object in db
+            User user = new User(firstName, lastName, email);
+//            UserDB.insert(user);
+            
+            // set User object in request object and set URL
+            request.setAttribute("user", user);
+            url = "/thanks.jsp";   // the "thanks" page
+        }
+        
+        // forward request and response objects to specified URL
+        getServletContext()
+            .getRequestDispatcher(url)
+            .forward(request, response);
+  
+    }    
+    @Override
+    protected void doGet(HttpServletRequest request, 
+                         HttpServletResponse response) 
+                         throws ServletException, IOException {
+        doPost(request, response);
+    }    
+
+}
